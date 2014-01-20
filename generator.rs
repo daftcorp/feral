88ab6a379a703cee25a8ff52c938f41dc1005086
Neil LoBracco <neil.lobracco@gmail.com>
2014-01-19 21:05:39 -0500

    I fought the type system and the type system won.

--- generator.rs
+++ generator.rs
@@ -2,6 +2,11 @@
 extern mod extra;
 use extra::json::{Json,Object,String,Error};
 use std::io::{File,Path,Reader};
+use std::hashmap::HashMap;
+struct RouteHandler {
+  controller : ~str,
+  action : ~str
+}
 fn main() {
   let contents = File::open(&Path::new("routes.json")).read_to_str();
   let result = extra::json::from_str(contents);
@@ -10,6 +15,19 @@ fn main() {
     Ok(Object(o)) => o,
     _ => fail!("Root must be object type!")
   };
-  let myblock = root.iter().map( |(k,v)| k).fold(~"",|acc,key| acc + *key + ~", ");
-  println!("{}", myblock);
+  let mut file = File::create(&Path::new("router.rs"));
+  let mut table = HashMap::<~str,~RouteHandler>::new();
+  for (k,v) in root.iter() {
+    let target: ~str = match *v {
+      String(s) => s,
+      _ => fail!("Route values must be strings!")
+    };
+    let vals: ~[&str] = target.split('#').collect();
+    let handler: ~RouteHandler = match vals {
+      [c,a] => &RouteHandler { controller : c.to_owned(), action : a.to_owned()},
+      _ => fail!("Handler must contain a '#' token!")
+    };
+    table.insert(target, handler);
+  }
+  file.write(bytes!("mod router{\nfnroute(url:~str) => Result<~Response>{\nmatch *str {\n"));
 }
