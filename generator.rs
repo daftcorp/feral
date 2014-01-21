725365115a98e0afde8543602032403320f10a59
Neil LoBracco <neil.lobracco@gmail.com>
2014-01-20 19:57:26 -0500

    woo kinda reads shit.

--- generator.rs
+++ generator.rs
@@ -1,7 +1,7 @@
 #[crate_id = "generator"];
 extern mod extra;
-use extra::json::{Json,Object,String,Error};
-use std::io::{File,Path,Reader};
+use extra::json::{Object,String};
+use std::io::{File};
 use std::hashmap::HashMap;
 struct RouteHandler {
   controller : ~str,
@@ -16,18 +16,26 @@ fn main() {
     _ => fail!("Root must be object type!")
   };
   let mut file = File::create(&Path::new("router.rs"));
-  let mut table = HashMap::<~str,~RouteHandler>::new();
+  file.write(bytes!("mod router{\nfnroute(url:~str) => Result<~Response>{\nmatch *str {\n"));
+  let table = toMap(root);
+  for (path, handler) in table.iter() {
+    file.write(format!("Route '{:s}' maps to controller '{:s}', action '{:s}'",*path,handler.controller,handler.action).as_bytes());
+  }
+}
+
+fn toMap(root: ~Object) -> ~HashMap<~str,RouteHandler> {
+  let mut table = HashMap::<~str,RouteHandler>::new();
   for (k,v) in root.iter() {
     let target: ~str = match *v {
-      String(s) => s,
+      String(ref s) => s.to_owned(),
       _ => fail!("Route values must be strings!")
     };
     let vals: ~[&str] = target.split('#').collect();
-    let handler: ~RouteHandler = match vals {
-      [c,a] => &RouteHandler { controller : c.to_owned(), action : a.to_owned()},
+    let handler = match vals {
+      [c,a] => RouteHandler { controller : c.to_owned(), action : a.to_owned()},
       _ => fail!("Handler must contain a '#' token!")
     };
-    table.insert(target, handler);
+    table.insert(k.to_owned(), handler);
   }
-  file.write(bytes!("mod router{\nfnroute(url:~str) => Result<~Response>{\nmatch *str {\n"));
+  ~table
 }
