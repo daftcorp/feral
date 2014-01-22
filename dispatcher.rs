1c1d940727749cb6462d7f71c5b030ee8d731ff2
Neil LoBracco <neil.lobracco@gmail.com>
2014-01-21 22:52:17 -0500

    just some stuff. move along.

--- dispatcher.rs
+++ dispatcher.rs
@@ -1,6 +1,15 @@
 mod dispatcher {
   mod router;
   pub fn dispatch_request(url: &str, body: &str) -> ~ServletResponse {
+    let sections = url.split('?').collect();
+    let (route,param_string) = match sections {
+      [r,p] => (r,p),
+      [r] => (r,()),
+      _ => fail!("Route must have only one ? character!")
+    };
+    let params = HashMap::new();
+    for assignment in param_string.split('&') {
+      let 
     let handler = router.getHandler(url);
     handler(body)
   }
